662fb76cd36815ad29bcc6aea616c64d63cf6689
Jesal1910 <77778144+Jesal1910@users.noreply.github.com>
2021-07-18 12:53:23 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,51 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.rank = 0;
+          // create the property for the score and intialize with zero
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    static updatePlayerRank(rank) {
+        database.ref('/').update({
+            playerRank: rank
+        });
+    }
+
+    getPlayerRank() {
+        var playerRankRef = database.ref('playerRank');
+        playerRankRef.on("value", (data) => {
+            this.rank = data.val();
+        })
+    }
+}
